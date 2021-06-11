b51a24922d080f984d1abb176504c4ac382279ae
Akaash Dev <akaashdev@Akaashs-Mac.local>
2021-06-11 18:35:04 +0530

    Calendar design ready

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,29 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('../pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+      },
+      {
+        path: 'classes',
+        loadChildren: () => import('../pages/classes/classes.module').then( m => m.ClassesPageModule)
+      },
+      {
+        path: 'calendar',
+        loadChildren: () => import('../pages/calendar/calendar.module').then( m => m.CalendarPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../pages/profile/profile.module').then( m => m.ProfilePageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/home/dashboard',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
